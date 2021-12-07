4a6f937d28ac7097e18e82401c57d58c2204b137
Peter <warrenp11@gmail.com>
2021-12-07 13:42:50 -0500

    compared the hashed password to user input. login route done

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -55,18 +55,25 @@ router.post("/login", (req, res) => {
   // expects {email, password}
   User.findOne({
     where: {
-      email: req.body.email
-    }
-  }).then(dbUserData => {
+      email: req.body.email,
+    },
+  }).then((dbUserData) => {
     if (!dbUserData) {
-      res.status(400).json({ message: 'No user with that email address!' });
+      res.status(400).json({ message: "No user with that email address!" });
       return;
     }
 
-    res.json({ user: dbUserData });
+    // res.json({ user: dbUserData });
 
     // Verify user
+    const validPassword = dbUserData.checkPassword(req.body.password);
 
+    if (!validPassword) {
+      res.status(400).json({ message: "Incorrect password!" });
+      return;
+    }
+
+    res.json({ user: dbUserData, message: "You are now logged in!" });
   });
 });
 

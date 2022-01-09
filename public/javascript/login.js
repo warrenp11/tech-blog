113220b32dd5806cd77273d0820a21113d18321b
Peter <warrenp11@gmail.com>
2022-01-09 17:00:02 -0500

    added functionality to signupFormHandler and loginFormHandler

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,5 +1,30 @@
+// Login form handler
+async function loginFormHandler(event) {
+  event.preventDefault();
+
+  const email = document.querySelector("#email-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
+
+  if (email && password) {
+    const response = await fetch("/api/users/login", {
+      method: "post",
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
+
 // Listen for submit event on form
-function signupFormHandler(event) {
+async function signupFormHandler(event) {
   event.preventDefault();
 
   // POST the username, email, and password from the form to our server
@@ -9,7 +34,7 @@ function signupFormHandler(event) {
 
   // make a fetch() POST request to the /api/users/ by updating the signupFormHandler() logic
   if (username && email && password) {
-    fetch("/api/users", {
+    const response = await fetch("/api/users", {
       method: "post",
       body: JSON.stringify({
         username,
@@ -17,11 +42,21 @@ function signupFormHandler(event) {
         password,
       }),
       headers: { "Content-Type": "application/json" },
-    }).then((response) => {
-      console.log(response);
     });
+    // console.log(response);
+
+    // check the response status
+    if (response.ok) {
+      console.log("success");
+    } else {
+      alert(response.statusText);
+    }
   }
 }
+
+document
+  .querySelector(".login-form")
+  .addEventListener("submit", loginFormHandler);
 
 document
   .querySelector(".signup-form")
